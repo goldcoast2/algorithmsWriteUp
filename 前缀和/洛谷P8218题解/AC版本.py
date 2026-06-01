1b85eecaef378be6533e55f281495a0347fe3eb8
goldcoast2 <2137649292@qq.com>
2026-06-01 17:07:30 +0800

    Delete 前缀和/洛谷P8218题解 directory

--- 前缀和/洛谷P8218题解/AC版本.py
+++
@@ -1,9 +0,0 @@
-n = int(input())  #序列长度
-a = list(map(int, input().split()))  #n 个正整数 a₁, a₂, ⋯, aₙ
-m = int(input())  #区间的数量
-pre=[0]*(n+1)  #初始化前缀和数列
-for j in range(n):
-    pre[j+1]=pre[j]+a[j]   #构造前缀和数列
-for _ in range(m):
-    l,r=map(int,input().split())  #所求区间
-    print(pre[r]-pre[l-1])  #两项相减就是所求区间子段和
--- 前缀和/洛谷P8218题解/MLE版本.py
+++
@@ -1,7 +0,0 @@
-n = int(input())  #序列长度
-a = list(map(int, input().split()))  #n 个正整数 a₁, a₂, ⋯, aₙ
-m = int(input())  #区间的数量
-for _ in range(m):
-    l, r = map(int, input().split())  #所求区间
-    total = sum(a[l-1:r])   #区间求和，内存开销太大，MLE
-    print(total)
